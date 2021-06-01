2d96d861abfe53db885e6849b020a36ecde5971f
Your Name <you@example.com>
2021-06-01 15:37:08 -0300

    fazendo parte da tabela e fazendo a tela pedido funcionar PARTE 1

--- src/java/utils/Utilidades.java
+++ src/java/utils/Utilidades.java
@@ -1,7 +1,9 @@
 package utils;
 
 import javax.faces.context.FacesContext;
-
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 /**
  *
  * @author João
@@ -18,5 +20,35 @@ public class Utilidades {
         return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().containsKey(chave);
         
     }
+
+    public static void addMessage(FacesMessage.Severity severity, String summary, String detail) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, summary, detail));
+    }
+    
+    public static boolean verificaExisteSessao()
+    {
+        return !FacesContext.getCurrentInstance().getExternalContext().getSessionMap().isEmpty();
+    }
+    
+    public static boolean verificaExisteRegistroSessao(String registro)
+    {
+        return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().containsKey(registro);
+    }
+    
+    public static Object recuperaRegistroSessao(String registro)
+    {
+        return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(registro);
+    }
+    
+    public static Object salvaRegistroSessao(String registro, Object objeto)
+    {
+        return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(registro, objeto);
+    }
+    
+    public static String getRealPath()
+    {
+        return ((ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext()).getRealPath("/");
+    }
     
 }
+
